c153735c65987d17a330466c169362c0ac071044
Andressa Gomes <dessaceeuazul@gmail.com>
2021-04-21 19:55:09 +0100

    New Classes
    Added new classes

---
+++ src/model/Movies.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Andressa Gomes
+ */
+public class Movies {
+    protected String title;
+    protected String description;
+    protected int runTime;
+    protected int rating;
+    protected String genre;
+    protected int quantities;
+    protected boolean availability;
+    
+    public Movies(){
+        
+    }
+}
